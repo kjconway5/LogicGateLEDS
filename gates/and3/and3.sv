953b41bf5ea54ee20b70619779b5cde5ba25f981
kjconway5 <153889901+kjconway5@users.noreply.github.com>
2025-12-17 18:08:39 -0800

    and3, nand2, nor2

---
+++ gates/and3/and3.sv
@@ -0,0 +1,13 @@
+module and3 (
+  input [0:0] a_i,
+  input [0:0] b_i,
+  input [0:0] d_i,
+  output [0:0] c_o
+);
+
+  // For Lab 2, you may use assign statements!
+  // Your code here:
+
+  assign c_o = a_i & b_i & d_i;
+
+endmodule
---
+++ gates/nor2/nor2.sv
@@ -0,0 +1,12 @@
+module nor2 (
+  input [0:0] a_i,
+  input [0:0] b_i,
+  output [0:0] c_o
+);
+
+  // For Lab 2, you may use assign statements!
+  // Your code here:
+
+  assign c_o = ~(a_i | b_i);
+
+endmodule
